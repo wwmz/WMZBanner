8f1487809b42b436491df42051030afdf3e0029d
mingzhou.wu <mingzhou.wu@xblgroup.com>
2020-12-12 21:12:07 +0800

    pod更新至1.1.7  支持xib wXibCellClassNamesSet

--- WMZBanner/Demo/电商播报/DemoDianshang.m
+++ WMZBanner/Demo/电商播报/DemoDianshang.m
@@ -76,7 +76,7 @@
     .wFrameSet(CGRectMake(10, BannerHeight/4+140, BannerWitdh-20, 50))
     .wMyCellClassNamesSet(@"KuaiBaoCell")
     //xib
-//    .wXibCellClassNamesSet(@"KuaiBaoCell")
+//   .wXibCellClassNamesSet(@"KuaiBaoCell")
     .wMyCellSet(^UICollectionViewCell *(NSIndexPath *indexPath, UICollectionView *collectionView, id model, UIImageView *bgImageView,NSArray*dataArr) {
                //自定义视图
         KuaiBaoCell *cell = (KuaiBaoCell *)[collectionView dequeueReusableCellWithReuseIdentifier:NSStringFromClass([KuaiBaoCell class]) forIndexPath:indexPath];
